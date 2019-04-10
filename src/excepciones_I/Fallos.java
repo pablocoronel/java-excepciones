73b276539b5b5ce4b59614c43f8b7bef11361f00
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-04-10 16:01:37 -0300

    excepciones 1

--- src/excepciones_I/Fallos.java
+++ src/excepciones_I/Fallos.java
@@ -1,10 +1,29 @@
 package excepciones_I;
 
+import javax.swing.JOptionPane;
+
 public class Fallos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] mi_matriz = new int[5];
+
+		mi_matriz[0] = 5;
+		mi_matriz[1] = 38;
+		mi_matriz[2] = 20;
+		mi_matriz[3] = -15;
+		mi_matriz[4] = 40;
+
+		for (int i = 0; i < mi_matriz.length; i++) {
+			System.out.println("Posicion: " + i + " = " + mi_matriz[i]);
+		}
+
+		// pedir datos personales
+		String nombre = JOptionPane.showInputDialog("ingrese su ombre");
+		int edad = Integer.parseInt(JOptionPane.showInputDialog("ingrese su edad"));
+
+		System.out.println(("El usuario " + nombre + " tiene " + edad + " años. El programa termino su ejecucion"));
 	}
 
 }
